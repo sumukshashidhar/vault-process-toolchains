4ed2e6d268b49b6d05853723c4f5cc50aa26b989
sumukshashidhar <sumuks2@illinois.edu>
2023-05-07 05:28:24 -0500

    dev: remove comments

--- tests/empty_file_removal.rs
+++ tests/empty_file_removal.rs
@@ -1,11 +1,11 @@
 mod utils {
     pub mod file_operations;
 }
-use env_logger::{Builder, Env};
+// use env_logger::{Builder, Env};
 #[test]
 fn empty_file_removal() {
     // Set the logger to the max_level trace
-    Builder::from_env(Env::default().default_filter_or("trace")).init();
+    // Builder::from_env(Env::default().default_filter_or("trace")).init();
     let dir1 = "./tests/data/empty_file_removal/";
     let dir2 = "./tests/data/empty_file_removal_ideal/";
     let result = utils::file_operations::compare_directories(dir1, dir2, 4);
